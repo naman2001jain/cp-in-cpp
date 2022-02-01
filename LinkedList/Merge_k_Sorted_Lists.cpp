7de1816d3786b82c9c66dab6b2a4335ad1681b0a
Naman jain <62708894+naman2001jain@users.noreply.github.com>
2022-02-02 00:25:02 +0530

    Create Merge_k_Sorted_Lists.cpp

---
+++ LinkedList/Merge_k_Sorted_Lists.cpp
@@ -0,0 +1,31 @@
+//problem link. https://leetcode.com/problems/merge-k-sorted-lists/
+
+class Solution {
+public:
+    
+    struct compare{
+        bool operator()(ListNode* l1, ListNode* l2){
+            return l1->val > l2->val;
+        }   
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*> , compare> pq;
+        ListNode* res = new ListNode(-1);
+        for(auto l:lists){
+            if(l) pq.push(l);
+        }
+        ListNode* dummy = new ListNode(-1);
+        ListNode* itr = dummy;
+        while(!pq.empty()){
+            ListNode* temp = pq.top();
+            pq.pop();
+            itr->next = temp;   
+            if(temp!=NULL && temp->next!=NULL)
+                pq.push(temp->next);
+            itr = itr->next;
+            
+        }
+        return dummy->next;
+    }
+};
